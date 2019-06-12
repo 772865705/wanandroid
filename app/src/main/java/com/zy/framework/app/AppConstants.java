3febf6008d01ababd83c5fb90f96d0aa3ff4c080
772865705 <772865705@qq.com>
2019-06-12 18:28:26 +0800

    init

---
+++ app/src/main/java/com/zy/framework/app/AppConstants.java
@@ -0,0 +1,6 @@
+package com.zy.framework.app;
+
+public class AppConstants {
+    public static boolean debug = false;
+    public static final String BASE_URL = "https://www.wanandroid.com";
+}
---
+++ app/src/main/java/com/zy/framework/app/MyApplication.java
@@ -0,0 +1,26 @@
+package com.zy.framework.app;
+
+import android.app.Application;
+
+import me.jessyan.autosize.AutoSizeConfig;
+
+public class MyApplication extends Application {
+
+    private static MyApplication app;
+    public static MyApplication getApp(){
+        return app;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        app = this;
+
+        initAutosize();
+
+    }
+
+    private void initAutosize(){
+        AutoSizeConfig.getInstance().setCustomFragment(true);
+    }
+}

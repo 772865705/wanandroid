efabae41a34aeed18332cd03cb2f4281839fa4d4
772865705 <772865705@qq.com>
2019-06-20 00:16:05 +0800

    feat:Main mvp

---
+++ app/src/main/java/com/zy/zywanandroid/ui/contract/MainContract.java
@@ -0,0 +1,15 @@
+package com.zy.zywanandroid.ui.contract;
+
+/**
+ * Created by ZhaoYue on 2019/6/20.
+ */
+public interface MainContract {
+    interface Model {
+    }
+
+    interface View {
+    }
+
+    interface Presenter {
+    }
+}
